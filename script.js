2217448000532f797989fa458ffd8466af6bba0a
Mateo <mateozguppy@gmail.com>
2023-06-03 23:31:01 -0400

    got the dot dot dot animation to work

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+
+
 const startGame = ((player1, player2, roll1, roll2) => {
     const startButton = document.querySelector('#start-button')
+    let diceRollText = document.querySelector("#dice-roll");
+    let dotCount = 0;
     function rollDiceAnimation() {
-        startButton.element = ""
-        startButton.innerText = "Rolling dice";
+       
+        if (dotCount < 3) {
+            dotCount += 1
+            diceRollText.textContent += " . ";
+        } else {
+            dotCount = 0;
+            diceRollText.textContent = "Rolling dice "
+        }
     }
     startButton.addEventListener('click', () => {
-        diceRollText = document.querySelector("#dice-roll");
         diceRollText.style.display = "block";
         startButton.style.display = "none";
+        setInterval(rollDiceAnimation, 250);
     })
 })();
